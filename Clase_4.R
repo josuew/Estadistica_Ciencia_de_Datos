68b99b2f2b9f5ae10d2348b02a041eb1472a60b5
a0487048 <j-jauregui@ti.com>
2025-10-24 16:58:39 -0600

    V.1.0.0

--- Clase_4.R
+++ Clase_4.R
@@ -11,11 +11,8 @@ dengue <- read.csv("data/dengue_abierto.csv")
 
 # Aqui se especifica que hoja leer
 conv_sexo <- iconv("CATÁLOGO SEXO", from = "latin1", to = "UTF-8")
-catalo_sexo <- read_excel("data/cat_dengue.xlsx", sheet = conv_sexo)
+catalo_sexo <- read_excel("data/cat_dengue.xlsx", sheet = "CATÁLOGO SEXO")
 catalo_sexo
-
-sheet_name2 <- iconv("CATÁLOGO MUNICIPIO", from = "latin1", to = "UTF-8")
-catalo_munici <- read_excel("data/cat_dengue.xlsx", sheet = sheet_name2)
 
 # catalo_sexo
 # dengue$SEXO
@@ -34,7 +31,7 @@ table(dengue2$SEXO, dengue2$`DESCRIPCIÓN`)
 #Catalogo Entidad
 # En este ejemplo la columna CLAVE_ENTIDAD parece numerica pero es "text"
 conv_entidad <- iconv("CATÁLOGO ENTIDAD", from = "latin1", to = "UTF-8")
-catalo_entidad <- read_excel("data/cat_dengue.xlsx", sheet = conv_entidad)
+catalo_entidad <- read_excel("data/cat_dengue.xlsx", sheet = "CATÁLOGO ENTIDAD")
 catalo_entidad
 
 # Convertimos la columna CLAVE_ENTIDAD a numerica para empatarla con ENTIDAD_RES que es numerica
@@ -44,6 +41,7 @@ catalo_entidad
 # Agregamos las columnas de la sheet "CATÁLOGO ENTIDAD" en la tabla que unimos con "CATÁLOGO SEXO"
 dengue3 <- merge(dengue2, catalo_entidad, by.x = "ENTIDAD_RES", by.y = "CLAVE_ENTIDAD")
 dengue3
+colnames(dengue3)
 
 table(dengue3$ABREVIATURA)
 
@@ -56,23 +54,41 @@ aggregate(EDAD_ANOS ~ ABREVIATURA + `DESCRIPCIÓN`, FUN = mean, data = dengue3)
 aggregate(EDAD_ANOS ~ `DESCRIPCIÓN` + ABREVIATURA, FUN = mean, data = dengue3)
 
 #Municipios
+conv_municipio <- iconv("CATÁLOGO MUNICIPIO", from = "latin1", to = "UTF-8")
+catalo_munici <- read_excel("data/cat_dengue.xlsx", sheet = "CATÁLOGO MUNICIPIO")
+
 catalo_munici$CLAVE_MUNICIPIO <- as.numeric(catalo_munici$CLAVE_MUNICIPIO)
 catalo_munici$CLAVE_ENTIDAD <- as.numeric(catalo_munici$CLAVE_ENTIDAD)
+catalo_munici
+
 
 dengue4 <- merge(x=dengue3, y=catalo_munici, by.x=c("ENTIDAD_RES","MUNICIPIO_RES"), by.y = c("CLAVE_ENTIDAD", "CLAVE_MUNICIPIO"), sort = FALSE)
+dengue4
+colnames(dengue4)
+
 dengue4$MUNICIPIO
 
-dengue4$muniunico <- paste0(dengue4$ABREVIATURA, "- ", dengue4$MUNICIPIO)
+# Genera la columna
+dengue4$muniunico <- paste0(dengue4$ABREVIATURA, " - ", dengue4$MUNICIPIO)
+dengue4$muniunico
+colnames(dengue4)
+
+# Estados - Municipio con mayores numeros de dengue
 sort(table(dengue4$muniunico), decreasing = TRUE)[1:10]
+
+# Estados con mayores numeros de dengue
 sort(table(dengue4$ABREVIATURA), decreasing = TRUE)[1:5]
 
-sum(dengue4[1,15:20]) # 12 significa qe no tiene enfermedad, 11 tiene 1, 10 tiene 2, etc...
+colnames(dengue4)[15:20]
+sum(dengue4[1,15:20]) # 12 significa que no tiene enfermedad, 11 tiene 1, 10 tiene 2, etc...
+
+# rowSums suma por renglones
 comorbilidades <- 12 - rowSums(dengue4[,15:20])
 tabla1 <- table(comorbilidades, dengue4$DEFUNCION)
+tabla1
 
 dengue4[which(comorbilidades == 6),]
 
-tabla1
 prop.table(tabla1,1) # Proporciones por fila
 prop.table(tabla1,2) # Proporciones por columna
 
